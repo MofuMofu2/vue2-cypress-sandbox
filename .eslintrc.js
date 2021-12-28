ebdf3d5b078b8056f9b8445eecfb87d5883e740a
MofuMofu2 <froakie002@gmail.com>
2021-12-28 21:04:17 +0900

    fix: Format on saveが効くようにする

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,10 +9,11 @@ module.exports = {
     "@vue/typescript/recommended",
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint",
+    "prettier",
   ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
+  plugins: ["@typescript-eslint"],
+  parser: "vue-eslint-parser",
+  parserOptions: { parser: "@typescript-eslint/parser" },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
