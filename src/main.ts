998b6bc0fa7d855e469b01bf2628edf951aef7f3
MofuMofu <froakie002@gmail.com>
2022-01-12 12:56:34 +0900

    feature: #2 npm run serve時にモックが立ち上がるようにした
    Vue側が.tsでmainの処理を書いているのでモックサーバー用の定義もTypeScriptでないと不整合がおこる

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import router from "./router";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import { BootstrapVue } from "bootstrap-vue";
+import { worker } from "./mocks/browser";
 
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
+
+if (process.env.NODE_ENV === "development") {
+  worker.start();
+}
 
 new Vue({
   router,
