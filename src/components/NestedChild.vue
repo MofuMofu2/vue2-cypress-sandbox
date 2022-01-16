13f20b8e7754e74d8c16f364fbd446b800e0017a
MofuMofu2 <froakie002@gmail.com>
2022-01-16 18:24:22 +0900

    fix: #8 add link

--- src/components/NestedChild.vue
+++ src/components/NestedChild.vue
@@ -1,16 +1,27 @@
 <template>
   <div id="child-component">
     <p>名前: {{ user.name }}</p>
-    <p>GitHub: {{ user.github }}</p>
+    <p>
+      GitHub: <a :href="githubUrl">{{ user.github }}</a>
+    </p>
     <p>Twitter: {{ user.twitter }}</p>
   </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
+import { Profile } from "@/interfaces/profile";
 export default Vue.extend({
   name: "NestedChild",
   props: {
-    user: Object,
+    user: { type: Object as () => Profile },
+  },
+  computed: {
+    githubUrl() {
+      return "https://github.com/" + this.user.github;
+    },
+    twitterUrl() {
+      return "https://twitter.com/" + this.user.twitter;
+    },
   },
 });
 </script>
---
+++ src/interfaces/profile.ts
@@ -0,0 +1,6 @@
+export type Profile = {
+  name: string;
+  github: string;
+  twitter: string;
+  [key: string]: string;
+};
