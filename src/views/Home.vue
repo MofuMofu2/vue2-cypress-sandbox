ee7de9d98e81ec5e3ab6c92e4d4460df49c9e5e9
MofuMofu2 <froakie002@gmail.com>
2022-01-16 16:41:33 +0900

    fix: 後のことを考えてpタグ入れる

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="home">りまりま団について紹介するページです。</div>
+  <div class="home">
+    <p>りまりま団について紹介するページです。</p>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-
 export default Vue.extend({
   name: "Home",
 });
