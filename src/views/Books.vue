4c0e4c9e6a5dfd6180963ba2f92400877a53e621
MofuMofu2 <froakie002@gmail.com>
2022-01-16 21:17:13 +0900

    feature: #10 APIリクエストでパラメータを取得してくる処理を追加

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -8,5 +8,10 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "Books",
+  mounted() {
+    fetch("/books")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  },
 });
 </script>
