3701200bfcb789884fe3ce3b08ad12a25d1ed22c
MofuMofu2 <froakie002@gmail.com>
2022-01-16 17:55:58 +0900

    fix: #8 センター寄せやめる

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,9 @@
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
-    <router-view></router-view>
+    <div class="mx-4 my-3">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -27,7 +29,6 @@ export default Vue.extend({
   font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page.</h1>
-    <p>Name: {{ user.name }}</p>
-    <p>GitHub: {{ user.github }}</p>
-    <p>Twitter: {{ user.twitter }}</p>
+    a
+    
   </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
 export default Vue.extend({
   name: "About",
-  props: {
-    user: Object,
-  },
 });
 </script>
