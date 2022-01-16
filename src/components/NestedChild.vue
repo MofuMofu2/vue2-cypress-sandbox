f24758b411b1f1601eeb7e0675779cad917cc27c
MofuMofu2 <froakie002@gmail.com>
2022-01-16 18:05:11 +0900

    fix: #8 無理やりネストコンポーネントを作ってみる（本当は1枚に纏めろ！というかんじ）

---
+++ src/components/NestedChild.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="child-component">
+    <p>名前: {{ user.name }}</p>
+    <p>GitHub: {{ user.github }}</p>
+    <p>Twitter: {{ user.twitter }}</p>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "NestedChild",
+  props: {
+    user: Object,
+  },
+});
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="about">
-    a
-    
+    <nested-sample />
   </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
+import NestedSample from "@/components/NestedSample.vue";
 export default Vue.extend({
   name: "About",
+  components: { NestedSample },
 });
 </script>
