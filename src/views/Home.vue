0e0214dc499c3d70fe57eaeb99a27447b6356000
MofuMofu2 <froakie002@gmail.com>
2022-01-16 15:43:09 +0900

    delete: #6 不要なコンポーネント削除

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,20 +3,14 @@
     <router-link :to="{ name: 'About', params: { profile: profile } }">
       About Page
     </router-link>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script lang="ts">
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import Vue from "vue";
 
 export default Vue.extend({
   name: "Home",
-  components: {
-    HelloWorld,
-  },
   data() {
     return {
       profile: {
