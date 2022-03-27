9a8d367ff5b8ce198e08b7643ecd7b1364a8c8b5
MofuMofu <froakie002@gmail.com>
2022-03-27 20:16:53 +0900

    fix: #23 複数タスク対応のためInterface変更

--- tests/unit/todolist.spec.ts
+++ tests/unit/todolist.spec.ts
@@ -10,13 +10,13 @@ describe("初期状態", () => {
     const left = wrapper.findAll("td").at(0);
     const content = left.find("input[type='checkbox']");
     expect(content.exists()).toBe(true);
-    expect(wrapper.vm.$data.completed).toBe(false);
+    expect(wrapper.vm.$data.tasks[0].completed).toBe(false);
   });
   it("中央は空のフォーム", () => {
     const central = wrapper.findAll("td").at(1);
     const content = central.find("input[type='text']");
     expect(content.exists()).toBe(true);
-    expect(wrapper.vm.$data.task).toBe("");
+    expect(wrapper.vm.$data.tasks[0].task).toBe("");
   });
   it("右端は削除ボタン", () => {
     const right = wrapper.findAll("td").at(2);
