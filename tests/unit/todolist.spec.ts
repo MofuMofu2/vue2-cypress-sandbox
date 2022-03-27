0388bf95680b6aafdfc7eb4dc0e1af157f4d3e0d
MofuMofu <froakie002@gmail.com>
2022-03-27 21:41:31 +0900

    fix: #23 エンターキー押下で初期化状態の行が追加されることを検証するためのテスト追加

--- tests/unit/todolist.spec.ts
+++ tests/unit/todolist.spec.ts
@@ -23,3 +23,21 @@ describe("初期状態", () => {
     expect(right.find("div").exists()).toBe(true);
   });
 });
+
+describe("新しいタスクを追加する", () => {
+  const wrapper = shallowMount(TodoList);
+  const firstRow = wrapper.findAll("tr").at(0);
+  const form = firstRow.findAll("td").at(1).find("input[type='text']");
+  beforeEach(async () => {
+    await form.setValue("new task");
+  });
+  it("フォームに値が入力できる", () => {
+    expect(wrapper.vm.$data.tasks[0].task).toBe("new task");
+  });
+  it("Enterキー押下で空行追加される", async () => {
+    await form.trigger("keyup.enter");
+    expect(wrapper.findAll("tr").length).toBe(2);
+    expect(wrapper.vm.$data.tasks[1].completed).toBe(false);
+    expect(wrapper.vm.$data.tasks[1].task).toBe("");
+  });
+});
