fa5e65003ceefaf3869f7000f8a03122c0bba020
MofuMofu <froakie002@gmail.com>
2022-03-29 22:09:51 +0900

    feature: #15 一番上の行（ヘッダー）に一括切り替え用のチェックを入れたい。theadとtbodyに分けることにする

--- tests/unit/todolist.spec.ts
+++ tests/unit/todolist.spec.ts
@@ -3,30 +3,31 @@ import TodoList from "@/components/TodoList.vue";
 
 describe("初期状態", () => {
   const wrapper = shallowMount(TodoList);
+  const body = wrapper.find("tbody");
   it("一行表示される", () => {
-    expect(wrapper.findAll("tr").length).toBe(1);
+    expect(body.findAll("tr").length).toBe(1);
   });
   it("左端はチェックボックス表示 チェック：OFF", () => {
-    const left = wrapper.findAll("td").at(0);
+    const left = body.findAll("td").at(0);
     const content = left.find("input[type='checkbox']");
     expect(content.exists()).toBe(true);
     expect(wrapper.vm.$data.tasks[0].completed).toBe(false);
   });
   it("中央は空のフォーム", () => {
-    const central = wrapper.findAll("td").at(1);
+    const central = body.findAll("td").at(1);
     const content = central.find("input[type='text']");
     expect(content.exists()).toBe(true);
     expect(wrapper.vm.$data.tasks[0].task).toBe("");
   });
   it("右端は削除ボタン", () => {
-    const right = wrapper.findAll("td").at(2);
+    const right = body.findAll("td").at(2);
     expect(right.find("div").exists()).toBe(true);
   });
 });
 
 describe("新しいタスクを追加する", () => {
   const wrapper = shallowMount(TodoList);
-  const firstRow = wrapper.findAll("tr").at(0);
+  const firstRow = wrapper.find("tbody").findAll("tr").at(0);
   const form = firstRow.findAll("td").at(1).find("input[type='text']");
   beforeEach(async () => {
     await form.setValue("new task");
@@ -44,7 +45,7 @@ describe("新しいタスクを追加する", () => {
 
 describe("タスクを削除する", () => {
   const wrapper = shallowMount(TodoList);
-  const deleteTarget = wrapper.findAll("tr").at(0);
+  const deleteTarget = wrapper.find("tbody").findAll("tr").at(0);
   const form = deleteTarget.findAll("td").at(1).find("input[type='text']");
 
   it("削除要素をクリックすると一行消える", async () => {
