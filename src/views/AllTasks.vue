88eaccbd3a4acfec7f39d7bf263828dc7c9d7452
MofuMofu <froakie002@gmail.com>
2022-04-07 23:21:50 +0900

    feature #19 TODOリストのインポート

--- src/views/AllTasks.vue
+++ src/views/AllTasks.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="all-tasks">
     <p>りまりま団について紹介するページです。</p>
+    <todo-list />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import TodoList from "@/components/TodoList.vue";
+
 export default Vue.extend({
+  components: { TodoList },
   name: "AllTasks",
 });
 </script>
